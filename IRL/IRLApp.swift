dfdb463529786ac450c3999858bcd0b8942134d4
Aus Heller <aus@onchaincommerce.io>
2025-08-18 17:32:25 -0700

    Initial Commit

---
+++ IRL/IRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRLApp.swift
+//  IRL
+//
+//  Created by Aus Heller on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct IRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IRLTests/IRLTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRLTests.swift
+//  IRLTests
+//
+//  Created by Aus Heller on 8/18/25.
+//
+
+import Testing
+@testable import IRL
+
+struct IRLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
